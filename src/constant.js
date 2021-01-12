050cec9174e64ddabcd4e73740b869b26ce56aed
ridoansaleh <ridoansalehnasution98@gmail.com>
2021-01-13 06:50:40 +0700

    add login page

---
+++ src/constant.js
@@ -0,0 +1,8 @@
+export const LOGIN_PATH = "/";
+export const REGISTRATION_PATH = "/registration";
+export const DASHBOARD_PATH = "/dashboard";
+export const STAFF_PATH = "/staff";
+
+const API_BASE_URL = "https://blooming-sierra-10191.herokuapp.com/";
+
+export const LOGIN_API = API_BASE_URL + "login";
