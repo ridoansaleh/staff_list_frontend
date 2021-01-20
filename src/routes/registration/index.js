88c58f3d39fb4288be9bf079d7134cf4a592c14a
RSN <ridoansalehnasution98@gmail.com>
2021-01-21 04:00:05 +0700

    Change favicon logo, add Header in registration page etc (#8)

--- src/routes/registration/index.js
+++ src/routes/registration/index.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { Form, Button, Message, Divider } from "semantic-ui-react";
 import { Helmet } from "react-helmet";
+import Header from "../../components/Header";
 import { Container } from "./_registrationStyle";
 import { REGISTRATION_API } from "../../constant";
 
@@ -91,88 +92,91 @@ function Registration() {
       <Helmet>
         <title>Staff List | Registration</title>
       </Helmet>
-      <h2>Company Details</h2>
-      <Divider />
-      <Form onSubmit={handleSubmission}>
-        <Form.Input
-          label="Company Name"
-          placeholder="Company Name"
-          value={companyName}
-          onChange={(e) => setCompanyName(e.target.value)}
-          error={isFormSubmitted && !companyName}
-        />
-        <Form.TextArea
-          label="Address"
-          placeholder="Address"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-          error={isFormSubmitted && !address}
-        />
-        <Form.Input
-          label="City"
-          placeholder="City"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-          error={isFormSubmitted && !city}
-        />
-        <Form.Input
-          label="Country"
-          placeholder="Country"
-          value={country}
-          onChange={(e) => setCountry(e.target.value)}
-          error={isFormSubmitted && !country}
-        />
-        <Form.Input
-          type="number"
-          label="Number of Employee"
-          placeholder="Number of Employee"
-          value={numberOfEmployee}
-          onChange={(e) => setNumberOfEmployee(e.target.value)}
-          error={isFormSubmitted && !numberOfEmployee}
-        />
-        <Form.Input
-          label="Industry"
-          placeholder="Industry"
-          value={industry}
-          onChange={(e) => setIndustry(e.target.value)}
-          error={isFormSubmitted && !industry}
-        />
-        <Form.Input
-          label="Admin Username"
-          placeholder="Admin Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          error={isFormSubmitted && !username}
-        />
-        <Form.Input
-          type="password"
-          label="Admin Password"
-          placeholder="Admin Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          error={isFormSubmitted && !password}
-        />
-        {isUsernameExist && (
-          <Message negative>
-            <Message.Header>Username already taken</Message.Header>
-            <p>Please use an unique username for your admin!</p>
-          </Message>
-        )}
-        {isSubmissionSucceed && (
-          <Message positive>
-            <Message.Header>Successfully Registered</Message.Header>
-            <p>Please login to start adding staffs to your company!</p>
-          </Message>
-        )}
-        <Button
-          type="submit"
-          loading={isLoading}
-          content="Sign Up"
-          fluid
-          secondary
-        />
-        <Button content="Cancel" fluid onClick={handleCancelClick} />
-      </Form>
+      <Header companyName="Staff List" isLogin={false} />
+      <section>
+        <h2>Company Details</h2>
+        <Divider />
+        <Form onSubmit={handleSubmission}>
+          <Form.Input
+            label="Company Name"
+            placeholder="Company Name"
+            value={companyName}
+            onChange={(e) => setCompanyName(e.target.value)}
+            error={isFormSubmitted && !companyName}
+          />
+          <Form.TextArea
+            label="Address"
+            placeholder="Address"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            error={isFormSubmitted && !address}
+          />
+          <Form.Input
+            label="City"
+            placeholder="City"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            error={isFormSubmitted && !city}
+          />
+          <Form.Input
+            label="Country"
+            placeholder="Country"
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+            error={isFormSubmitted && !country}
+          />
+          <Form.Input
+            type="number"
+            label="Number of Employee"
+            placeholder="Number of Employee"
+            value={numberOfEmployee}
+            onChange={(e) => setNumberOfEmployee(e.target.value)}
+            error={isFormSubmitted && !numberOfEmployee}
+          />
+          <Form.Input
+            label="Industry"
+            placeholder="Industry"
+            value={industry}
+            onChange={(e) => setIndustry(e.target.value)}
+            error={isFormSubmitted && !industry}
+          />
+          <Form.Input
+            label="Admin Username"
+            placeholder="Admin Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            error={isFormSubmitted && !username}
+          />
+          <Form.Input
+            type="password"
+            label="Admin Password"
+            placeholder="Admin Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            error={isFormSubmitted && !password}
+          />
+          {isUsernameExist && (
+            <Message negative>
+              <Message.Header>Username already taken</Message.Header>
+              <p>Please use an unique username for your admin!</p>
+            </Message>
+          )}
+          {isSubmissionSucceed && (
+            <Message positive>
+              <Message.Header>Successfully Registered</Message.Header>
+              <p>Please login to start adding staffs to your company!</p>
+            </Message>
+          )}
+          <Button
+            type="submit"
+            loading={isLoading}
+            content="Sign Up"
+            fluid
+            secondary
+          />
+          <Button content="Cancel" fluid onClick={handleCancelClick} />
+        </Form>
+      </section>
     </Container>
   );
 }
